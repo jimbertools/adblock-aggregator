e20b5cdec80b57d082a63fcc345650bfcbd0944d
azertyalex <azertyalex@hotmail.com>
2021-07-22 11:50:51 +0200

    use extra params

--- create_new_list.sh
+++ create_new_list.sh
@@ -1,6 +1,10 @@
 #/bin/bash
-
+if [ -z "$1" ] && [ -z "$2" ]; then
+    echo "usage: ./create_new_list.sh <path/to/file> /var/www/html/adblock.txt for production"
+    exit
+fi
+path=$1
 # curl --silent https://badmojr.github.io/1Hosts/Pro/adblock.txt 2>&1 | grep "||" | sed 's/\^//' | sed 's/\./\\./g' | sed 's/||/^.*/' >adblock.txt
-curl --silent https://badmojr.github.io/1Hosts/Pro/adblock.txt 2>&1 | grep "||" | sed 's/\^//' | sed 's/||//' >adblock.txt
-sed -i '/advertising-module.api.dpgmedia.cloud/d' adblock.txt
+curl --silent https://badmojr.github.io/1Hosts/Pro/adblock.txt 2>&1 | grep "||" | sed 's/\^//' | sed 's/||//' >$path
+sed -i '/advertising-module.api.dpgmedia.cloud/d' $path
 # cat adblock.txt.1 2>&1 | grep "||" | sed 's/\^//' | sed 's/||//' >adblock.txt
