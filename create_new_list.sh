9f5e80beb520694667d983c62e61d0d7e516959b
azertyalex <azertyalex@hotmail.com>
2021-07-22 11:46:42 +0200

    add extra sed's

--- create_new_list.sh
+++ create_new_list.sh
@@ -1,3 +1,6 @@
 #/bin/bash
 
-curl -v --silent https://badmojr.github.io/1Hosts/Pro/adblock.txt 2>&1 | grep "||" | cut -c 3- | sed 's/\^//' >adblock.txt
+# curl --silent https://badmojr.github.io/1Hosts/Pro/adblock.txt 2>&1 | grep "||" | sed 's/\^//' | sed 's/\./\\./g' | sed 's/||/^.*/' >adblock.txt
+curl --silent https://badmojr.github.io/1Hosts/Pro/adblock.txt 2>&1 | grep "||" | sed 's/\^//' | sed 's/||//' >adblock.txt
+sed -i '/advertising-module.api.dpgmedia.cloud/d' adblock.txt
+# cat adblock.txt.1 2>&1 | grep "||" | sed 's/\^//' | sed 's/||//' >adblock.txt
